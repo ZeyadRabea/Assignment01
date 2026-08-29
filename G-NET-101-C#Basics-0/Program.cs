af932820d3ec4079954365a73199db1633e9e97a
Ziad Rabea <zeyad.rabea@gmail.com>
2026-08-29 12:42:51 +0300

    Add project files.

---
+++ G-NET-101-C#Basics-0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_101_C_Basics_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
